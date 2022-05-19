da9351b4dfcdc90e1a27ae0ee9b729fce79c0505
Marcell-039 <marcellinoknl@gmail.com>
2022-05-19 09:22:33 +0700

    data admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('ktp');
+            $table->string('nama_lengkap');
+            $table->bigInteger('nohp');
             $table->string('email')->unique();
+            $table->string('google')->unique()->nullable();
+            $table->string('facebook')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
